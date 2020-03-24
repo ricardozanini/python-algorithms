814d1d398fc14c12fcf83833dc813f90bd5579d1
Ricardo Zanini <ricardozanini@gmail.com>
2020-03-24 18:38:37 -0300

    cleaning up and removing unnecessary code

--- hanoi_towers.py
+++ hanoi_towers.py
@@ -4,10 +4,12 @@ def get_last(tower):
     return tower[len(tower) - 1]
 
 
-def find_suitable_tower(source, towers=[]):
+def find_suitable_tower(source, last_move, towers=[]):
+    if get_last(towers[source]) == last_move:
+        return -1
+
     next_tower = get_next_tower(source, towers)
     next_next_tower = get_next_tower(next_tower, towers)
-
     if len(towers[next_tower]) == 0:
         return next_tower
     if get_last(towers[source]) == get_last(towers[next_tower]) - 1:
@@ -31,27 +33,20 @@ def get_next_tower(actual, towers):
     return actual + 1
 
 
-def move_discs(n, moves=0, actual=0, last_move=0, towers=[]):
-    if n == 1:
-        destination = find_suitable_tower(actual, towers)
-        if destination > -1:
-            if last_move != get_last(towers[actual]):
-                moves += 1
-                last_move = towers[actual].pop()
-                towers[destination].append(last_move)
-        return moves, last_move, towers
-
+def move_discs(moves=0, actual=0, last_move=0, towers=[]):
     while len(towers[actual]) > 0:
-        moves_before = moves
-        moves, last_move, towers = move_discs(
-            n - 1, moves, actual, last_move, towers)
-        # nothing changed
-        if moves == moves_before:
+        destination = find_suitable_tower(actual, last_move, towers)
+        if destination > -1: # we've found a possible movement for this tower
+            moves += 1
+            last_move = towers[actual].pop()
+            towers[destination].append(last_move)
+        else:  # nothing changed, let's try to clean up the next tower
             next_tower = get_next_tower(actual, towers)
+            # emptying the next tower, this way we can have some room to arrange our discs
             if len(towers[next_tower]) > 0:
                 moves, last_move, towers = move_discs(
-                    n, moves, next_tower, last_move, towers)
-            else:
+                    moves, next_tower, last_move, towers)
+            else: # no way out, we did what we could, this tower has no movements left
                 break
 
     return moves, last_move, towers
@@ -60,11 +55,11 @@ def move_discs(n, moves=0, actual=0, last_move=0, towers=[]):
 def move_tower(n, moves=0, towers=[]):
     last_move = 0
     for i in range(len(towers)):
+        # break the loop once we got the solution
         if i > 0 and len(towers[i]) == n:
             return moves, towers
-        if not (i > 0 and len(towers[i]) == 1 and get_last(towers[i]) == n):
-            moves, last_move, towers = move_discs(
-                n, moves, i, last_move, towers)
+        # keep trying to move the discs from the current tower
+        moves, last_move, towers = move_discs(moves, i, last_move, towers)
 
     return move_tower(n, moves, towers=towers)
 
